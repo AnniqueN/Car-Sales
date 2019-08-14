3c7b75c1c184789f4b8a3840f8e8a7267ddc8705
Annique Nsabimana <anniquensabimana@gmail.com>
2019-08-14 17:35:12 -0500

    Completed: MVP met

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import AdditionalFeature from './AdditionalFeature';
 
 const AdditionalFeatures = props => {
   return (
-    <div className="content">
+    <div className='content'>
       <h4>Additional Features</h4>
       {props.store.length ? (
-        <ol type="1">
+        <ol type='1'>
           {props.store.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
-        <p>Nice looking car!</p>
+        <p>BAUTIFAUL AUTOMOBILE!</p>
       )}
     </div>
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return {
+    store: state.store,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {},
+)(AdditionalFeatures);
